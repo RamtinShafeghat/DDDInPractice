7ccdb0258c5b8bc1f3c1c43cae2bb8d10cf7110d
Ramtin-sht <ramtin.shafeghat@gmail.com>
2023-08-27 16:58:02 +0330

    Update to .net 4.8

---
+++ DddInPractice.Logic/Atms/BalanceChangedEventHandler.cs
@@ -0,0 +1,12 @@
+﻿using DddInPractice.Logic.Common;
+
+namespace DddInPractice.Logic.Atms
+{
+    internal class BalanceChangedEventHandler : IHandler<BalanceChangedEvent>
+    {
+        public void Handle(BalanceChangedEvent domainEvent)
+        {
+            //submit it to message bus and receive it on another microservice
+        }
+    }
+}
